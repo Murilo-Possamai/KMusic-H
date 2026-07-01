4c7bf0f9d729203ab141e976e8f723f7b6f81c44
Gabriel Lucas <153223962+dhtrgabriell@users.noreply.github.com>
2026-07-01 17:43:29 -0300

    Views: Home and Playlist

---
+++ kmusich/lib/models/playlist.dart
@@ -0,0 +1,13 @@
+class Playlist {
+  final String title;
+  final String speedCategory;
+  final String targetSpeed;
+  final String coverAsset;
+
+  const Playlist({
+    required this.title,
+    required this.speedCategory,
+    required this.targetSpeed,
+    required this.coverAsset,
+  });
+}
--- kmusich/lib/views/main_page.dart
+++ kmusich/lib/views/main_page.dart
@@ -1,41 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:kmusich/auth_service.dart';
+import 'package:kmusich/views/home_view.dart';
 import 'package:kmusich/views/login_page.dart';
+import 'package:kmusich/views/playlist_view.dart';
+import 'package:kmusich/widgets/custom_buttom_nav.dart';
+import 'package:kmusich/widgets/custom_top_bar.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
-  void sair(BuildContext context) {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _views = [
+    const HomeView(),
+    const PlaylistView(),
+    const Center(
+      child: Text(
+        'Configurações',
+        style: TextStyle(color: Colors.white, fontSize: 24),
+      ),
+    ),
+  ];
+
+  /* void sair(BuildContext context) {
     authService.value.signOut();
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => const LoginPage()),
     );
-
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Tela Principal',
-              style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () => sair(context),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const CustomTopBar(),
+              const SizedBox(height: 16),
+
+              Expanded(
+                child: IndexedStack(index: _currentIndex, children: _views),
               ),
-              child: const Text('Sair'),
-            ),
-          ],
+
+              CustomBottomNav(
+                currentIndex: _currentIndex,
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+
+              const SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
